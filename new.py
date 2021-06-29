513854c509a0e2dcf063c26573f87143bfac872d
OGB123 <86609011+OGB123@users.noreply.github.com>
2021-06-29 12:18:57 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,2 +1,10 @@
 print ("hello")
-Print str(insert("what is you user name "))
+Username str(insert("what is you user name "))
+Username=username
+if username=ogb:
+  Password int(insert("please enter you password "))
+  Password=password
+  if password=1234:
+    print ("loggin in")
+  else:
+    Print ("please Renter your username and password")
